462b123c346f09d456db9ff469d4730590193f4a
Valence <office@valencex.com>
2018-12-16 20:18:58 +0800

    node.List tests

--- node_test.go
+++ node_test.go
@@ -6,9 +6,89 @@
 
 package main
 
-// node CRUD
-// node insert seq
-// node insert parallel
-// node list
-// node properties
-// node bad signature (CRUD)
+import (
+	pb "cds.ikigai.net/cabinet.v1/rpc"
+	"fmt"
+	"io"
+	"testing"
+)
+
+func TestNodeCreateList(t *testing.T) {
+	it := CabinetTest{test: t}
+	it.setup(4)
+
+	nPayloads := make(map[string][]byte)
+	trx := make([]pb.TransactionAction, 0)
+	pos := uint32(0)
+
+	nSeq, err  := it.client.SequentialCreate(it.ctx, &pb.Sequential{Type: "n", Uuid: MockRandomUUID()})
+	it.logThing("SeqCreate", err, "SequentialCreate")
+	nType := nSeq.Seqid
+
+	// create nodes
+	for pos < TestSequentialSize{
+		tPos := fmt.Sprintf("tmp:%d", pos)
+		nPayloads[tPos] = MockRandomPayload()
+
+		trx = append(trx, pb.TransactionAction{
+			ActionId: pos, Action: &pb.TransactionAction_NodeCreate{NodeCreate: &pb.Node{
+				Type: nType, Version: 1, Id: tPos, Properties: nPayloads[tPos],
+		}}},)
+
+		pos += 1
+	}
+
+	mapIDs := CDSTransactionRunner(&trx, &it)
+	rMap := make(map[string]string)
+
+	for tmp, aID := range mapIDs{
+		rMap[aID] = tmp
+	}
+
+	// check list
+	lStr, err := it.client.NodeList(it.ctx, &pb.NodeListRequest{
+		NodeType: nType, IncludeType: true, IncludeId: true, IncludeProp: true, Opt: &pb.ListOptions{
+			Mode: pb.ListRange_ALL, PageSize: 100,
+	},})
+
+	if err != nil{
+		it.test.Errorf("[E] %v.NodeList(%d) = _. %v", it.client, nType, err)
+	}else{
+		for {
+			node, err := lStr.Recv()
+
+			if err == io.EOF {
+				break
+			}else if err != nil {
+				it.test.Errorf("[E] %v.SequentialList(_) = _, %v", it.client, err)
+				break
+			}else {
+				tmpPos := rMap[node.Id]
+
+				if node.Type != nType {
+					it.test.Errorf("[E] node.type got %d expected %d", node.Type, nType)
+				}else if node.Id != mapIDs[rMap[node.Id]] {
+					it.test.Errorf("[E] node.id got %s expected %s", node.Id, mapIDs[rMap[node.Id]])
+				}else if string(node.Properties) != string(nPayloads[tmpPos]) {
+					it.test.Errorf("[E] sequence.prop got %v expected %v", string(node.Properties), string(nPayloads[tmpPos]))
+				}else{
+					it.test.Logf("[I] %v.NodeList(%d) got %v", it.client, nType, node)
+				}
+			}
+		}
+	}
+
+	// clear records
+	trxDelete := make([]pb.TransactionAction, 0)
+
+	for _, nID := range mapIDs{
+		trxDelete = append(trxDelete, pb.TransactionAction{
+			ActionId: pos, Action: &pb.TransactionAction_NodeDelete{NodeDelete: &pb.Node{
+				Type: nType, Id: nID,
+			}}},)
+	}
+
+	_ = CDSTransactionRunner(&trx, &it)
+
+	it.tearDown()
+}
