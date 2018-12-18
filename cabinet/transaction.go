41b5e0554c93e6a3406316a77b6bc6ee4baee1d1
Valence <office@valencex.com>
2018-12-18 13:06:05 +0800

    T.ReadCheck tests; cds.Transaction runner tool

---
+++ cabinet/transaction.go
@@ -0,0 +1,162 @@
+// Package: net.ikigai.cds
+// Module: cabinet.services.test
+//
+// Author: Narcis M. PAP
+// Copyright (c) 2018 Ikigai Cloud. All rights reserved.
+
+package cabinet
+
+import (
+	pb "cds.ikigai.net/cabinet.v1/rpc"
+	"context"
+	"fmt"
+	"io"
+	"sync"
+)
+
+const (
+	TRANSACTION_ERROR_CONN = 0
+	TRANSACTION_ERROR_CLOSING = 1
+	TRANSACTION_ERROR_SENDING = 2
+	TRANSACTION_ERROR_RESPONSE = 3
+
+	TRANSACTION_ERROR_OPERATION = 10
+	TRANSACTION_ERROR_EMPTY = 11
+)
+
+type TransactionError struct {
+	msg 	string
+	class 	int
+}
+
+func (e *TransactionError) Error() string {
+	return fmt.Sprintf("ERR(%d): %s", e.class, e.msg)
+}
+
+type Transaction struct{
+	actions 	map[uint32]*pb.TransactionAction
+	response 	map[uint32]*pb.TransactionActionResponse
+	actionIDs	[]uint32
+	resMux 		sync.Mutex
+	actPos		uint32
+
+	queueErr 	[]error
+
+	idMap 	map[string]string
+	tmpMap 	map[uint32]string
+	mapMux 	sync.Mutex
+
+	resError error
+	resErrorMux sync.Mutex
+
+	client 	pb.CDSCabinetClient
+	ctx 	context.Context
+}
+
+func (c *Transaction) Operation(o *pb.TransactionAction){
+	if _, inActions := c.actions[o.ActionId]; inActions{
+		c.queueErr = append(c.queueErr, &TransactionError{msg: "duplicate actionID", class: TRANSACTION_ERROR_OPERATION})
+		return
+	}
+
+	c.actions[o.ActionId] = o
+	c.actionIDs = append(c.actionIDs, o.ActionId)
+	c.actPos += 1
+}
+
+func (c *Transaction) O(o *pb.TransactionAction){
+	o.ActionId = c.actPos
+	c.Operation(o)
+}
+
+func (c *Transaction) Pos() uint32{
+	return c.actPos
+}
+
+func (c *Transaction) Setup(ctx context.Context, cli pb.CDSCabinetClient){
+	c.actions 	= make(map[uint32]*pb.TransactionAction)
+	c.response 	= make(map[uint32]*pb.TransactionActionResponse)
+	c.queueErr  = make([]error, 0)
+	c.actionIDs = make([]uint32, 0)
+
+	c.idMap 	= make(map[string]string)
+	c.tmpMap 	= make(map[uint32]string)
+	c.actPos 	= uint32(1)
+
+	c.client 	= cli
+	c.ctx 		= ctx
+
+	c.resError 	= nil
+}
+
+
+func (c *Transaction) Commit() error{
+	if len(c.actions) == 0{
+		return &TransactionError{msg: "no queued transactions", class: TRANSACTION_ERROR_EMPTY}
+	}else if len(c.queueErr) > 0{
+		for er := range c.queueErr{
+			return c.queueErr[er]
+		}
+	}
+
+	stream, err := c.client.Transaction(c.ctx)
+
+	if err != nil{
+		return &TransactionError{msg: fmt.Sprintf("connection error: %s", err), class: TRANSACTION_ERROR_CONN}
+	}
+
+	wc := make(chan struct{})
+
+	go func(){
+		for {
+			actionResponse, err := stream.Recv()
+			//fmt.Printf("T.(receive) = %v, %v\n", actionResponse, err)
+
+			if err == io.EOF {
+				close(wc); return
+			}else if err != nil {
+				c.resErrorMux.Lock()
+				c.resError = &TransactionError{msg: fmt.Sprintf("%s", err), class: TRANSACTION_ERROR_RESPONSE}
+				c.resErrorMux.Unlock()
+
+				close(wc); return
+			}else {
+				c.resMux.Lock()
+				c.response[actionResponse.ActionId] = actionResponse
+				c.resMux.Unlock()
+
+				switch tReq := actionResponse.Response.(type) {
+				case *pb.TransactionActionResponse_NodeCreate:
+					c.mapMux.Lock()
+					c.idMap[c.tmpMap[actionResponse.ActionId]] = tReq.NodeCreate.Id
+					c.mapMux.Unlock()
+				}
+			}
+		}
+	}()
+
+	for _, aID := range c.actionIDs {
+		fmt.Printf("T.(send) %v\n", c.actions[aID])
+
+		if err := stream.Send(c.actions[aID]); err != nil {
+			return &TransactionError{msg: fmt.Sprintf("sending error: %s", err), class: TRANSACTION_ERROR_SENDING}
+		}
+
+		switch tReq := c.actions[aID].Action.(type) {
+		case *pb.TransactionAction_NodeCreate:
+			c.mapMux.Lock()
+			c.tmpMap[c.actions[aID].ActionId] = tReq.NodeCreate.Id
+			c.mapMux.Unlock()
+		}
+	}
+
+	err = stream.CloseSend()
+
+	if err != nil{
+		return &TransactionError{msg: fmt.Sprintf("close conn error: %s", err), class: TRANSACTION_ERROR_CLOSING}
+	}
+
+	<-wc
+
+	return c.resError
+}
--- transaction_readcheck_test.go
+++ transaction_readcheck_test.go
@@ -5,3 +5,99 @@
 // Copyright (c) 2018 Ikigai Cloud. All rights reserved.
 
 package main
+
+import (
+	"cds.ikigai.net/cabinet.v1.test/cabinet"
+	pb "cds.ikigai.net/cabinet.v1/rpc"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+// No need to do very comprehensive T.ReadCheck tests as that is taken care of by read_check.go file
+// T.ReadCheck is just a wrapper around using an existing transaction for live integrity testing
+
+func TestTransactionReadCheckNode(t *testing.T) {
+	it := CabinetTest{test: t}
+	it.setup(2)
+
+	p1, p2 := MockRandomPayload(), MockRandomPayload()
+	n1 := &pb.Node{Type: uint32(MockRandomInt(1, 65000)), Version: 1, Id: "tmp:1", Properties: p1}
+
+	mapIDs := CDSTransactionRunner(&([]pb.TransactionAction{
+		{ActionId: 1, Action: &pb.TransactionAction_NodeCreate{NodeCreate: n1}},
+	}), &it)
+
+	n1.Id = mapIDs["tmp:1"]
+	n1IRI := fmt.Sprintf("n/%d/%s", n1.Type, n1.Id)
+
+	cds := cabinet.Transaction{}
+	cds.Setup(it.ctx, it.client)
+
+	err := func() error{
+		cds.O(&pb.TransactionAction{
+			Action: &pb.TransactionAction_ReadCheck{ReadCheck: &pb.ReadCheckRequest{
+				Source: n1IRI, Operator: pb.CheckOperators_EQUAL, Target: &pb.CheckTarget{Target: &pb.CheckTarget_Val{Val: "not good"}},
+			}}})
+
+		cds.O(&pb.TransactionAction{
+			Action: &pb.TransactionAction_NodeUpdate{NodeUpdate: nodeWithPayload(n1, p2)},
+		})
+
+		return cds.Commit()
+	}()
+
+	checkTransactionFailed(&it, err, "E(0x013)")
+
+	// should still have p1
+	el1, err := it.client.NodeGet(it.ctx, &pb.NodeGetRequest{NodeType: n1.Type, Id: n1.Id})
+	it.logThing(el1, err, "NodeGet")
+	validatePayload(el1, &it, p1, el1.Properties)
+
+	// update payload with valid R/C, now payload is p2
+	cds2 := cabinet.Transaction{}
+	cds2.Setup(it.ctx, it.client)
+
+	cds2.O(&pb.TransactionAction{
+		Action: &pb.TransactionAction_ReadCheck{ReadCheck: &pb.ReadCheckRequest{
+			Source: n1IRI, Operator: pb.CheckOperators_EQUAL, Target: &pb.CheckTarget{Target: &pb.CheckTarget_Val{Val:string(p1)}},
+		}}})
+
+	cds2.O(&pb.TransactionAction{
+		Action: &pb.TransactionAction_ReadCheck{ReadCheck: &pb.ReadCheckRequest{
+			Source: n1IRI, Operator: pb.CheckOperators_EXISTS, Target: &pb.CheckTarget{Target: &pb.CheckTarget_Val{Val:"*"}},
+		}}})
+
+	cds2.O(&pb.TransactionAction{Action: &pb.TransactionAction_ReadCheck{ReadCheck: &pb.ReadCheckRequest{
+		Source: n1IRI, Operator: pb.CheckOperators_TOUCH, Target: &pb.CheckTarget{Target: &pb.CheckTarget_Val{Val:"*"}},
+	}}})
+
+	cds2.O(&pb.TransactionAction{Action: &pb.TransactionAction_NodeUpdate{
+		NodeUpdate: nodeWithPayload(n1, p2),
+	}})
+
+	err2 := cds2.Commit()
+	checkTransactionSuccess(&it, err2)
+
+	el2, err := it.client.NodeGet(it.ctx, &pb.NodeGetRequest{NodeType: n1.Type, Id: n1.Id})
+	it.logThing(el2, err, "NodeGet")
+	validatePayload(el2, &it, p2, el2.Properties)
+
+	it.tearDown()
+}
+
+func checkTransactionFailed(it *CabinetTest, err error, code string){
+	if err != nil && strings.Contains(err.Error(), code){
+		it.test.Logf("Transaction was rejected with code: %s", code)
+	}else{
+		it.test.Errorf("Transaction was not rejected with %s, got %v", code, err)
+	}
+}
+
+func checkTransactionSuccess(it *CabinetTest, err error){
+	if err != nil{
+		it.test.Errorf("Transaction failed with error: %s", err)
+	}else{
+		it.test.Log("Transaction was committed successfully")
+	}
+}
